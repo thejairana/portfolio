32a78d99be07b5a408b9fa06cf96578f1d92d979
jay.kumar <jay.kumar@elogist.in>
2019-05-29 10:08:02 +0530

    Updates

---
+++ precache-manifest.6eab032b411751a11c07c19c8c8ff64a.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc34b4351cfb754b37df131a0cbd336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1beda4a53df57d8697de",
+    "url": "/static/css/2.37e4971f.chunk.css"
+  },
+  {
+    "revision": "b4bd69fb1cbb800dd3f9",
+    "url": "/static/css/main.17e57656.chunk.css"
+  },
+  {
+    "revision": "1beda4a53df57d8697de",
+    "url": "/static/js/2.4f02e00a.chunk.js"
+  },
+  {
+    "revision": "b4bd69fb1cbb800dd3f9",
+    "url": "/static/js/main.4bfe33c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "98ad8554b7cd7a2e3ac9aa38bf286008",
+    "url": "/static/media/app-testing-automation.98ad8554.png"
+  },
+  {
+    "revision": "df99eca95acc83511d006fc4759b7b8d",
+    "url": "/static/media/banking-automation.df99eca9.png"
+  },
+  {
+    "revision": "1feff85a65b99d2240ca5c4b5a3f74d2",
+    "url": "/static/media/blogspire.1feff85a.jpg"
+  },
+  {
+    "revision": "adaa878b2b6946cfdda4ab2a97e7bdb5",
+    "url": "/static/media/booster-admin.adaa878b.png"
+  },
+  {
+    "revision": "1f8e3b969c64975ed295f4f23838b984",
+    "url": "/static/media/booster.1f8e3b96.png"
+  },
+  {
+    "revision": "77212c1944a9c4ea8c072409a0bb9ef6",
+    "url": "/static/media/cordova-plugin.77212c19.jpg"
+  },
+  {
+    "revision": "bbe29a6cf61aa91ac3aff1df40a3c2ca",
+    "url": "/static/media/css3.bbe29a6c.svg"
+  },
+  {
+    "revision": "aa845c1fd1e622c41669ba4b730aa3b9",
+    "url": "/static/media/dark-denim-3.aa845c1f.png"
+  },
+  {
+    "revision": "c81bf580d3bb16076deb7eb0e5e748af",
+    "url": "/static/media/default.c81bf580.jpg"
+  },
+  {
+    "revision": "41f7eb566fa3bc615bad319e9227402e",
+    "url": "/static/media/dost-app.41f7eb56.png"
+  },
+  {
+    "revision": "ef78fd175ecb26bfddea7a0b79c2216f",
+    "url": "/static/media/dost-dashboard.ef78fd17.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "11566bd9e9d133e31f7eaaa703fa7934",
+    "url": "/static/media/getshortcut.11566bd9.png"
+  },
+  {
+    "revision": "7c89237023385f0e515b3630d1136d6a",
+    "url": "/static/media/home-bg-1.7c892370.jpg"
+  },
+  {
+    "revision": "f449fdd3c76033aca9115399727e0412",
+    "url": "/static/media/home-bg.f449fdd3.jpg"
+  },
+  {
+    "revision": "f106a1acf3801e31a17f177033ad5cc4",
+    "url": "/static/media/ionic.f106a1ac.png"
+  },
+  {
+    "revision": "15a4866e67b5a9aac43ae3af08de6721",
+    "url": "/static/media/js.15a4866e.png"
+  },
+  {
+    "revision": "251edafef4d78056df3dfbdbb9815d09",
+    "url": "/static/media/js.251edafe.jpg"
+  },
+  {
+    "revision": "00b924bd0650c832b7ef9942c1a3904e",
+    "url": "/static/media/load-intelligence.00b924bd.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "22a4b42328ad1cd3dbc210237db327ba",
+    "url": "/static/media/mongo.22a4b423.png"
+  },
+  {
+    "revision": "ac943f47ec0f05f4daefc18e74927c00",
+    "url": "/static/media/myjoyride.ac943f47.webp"
+  },
+  {
+    "revision": "e2a0148cbf519ecd264a27d519c7453b",
+    "url": "/static/media/mylib.e2a0148c.webp"
+  },
+  {
+    "revision": "c502658a509d27b53679b3ef73c0d82f",
+    "url": "/static/media/nodejs.c502658a.png"
+  },
+  {
+    "revision": "4d7f4617db6532faa142c2e4d0335f85",
+    "url": "/static/media/peak-ok-website.4d7f4617.png"
+  },
+  {
+    "revision": "eb37770da1b1b3de38f319c71055cde8",
+    "url": "/static/media/photo-1496954649918-80cc4e7a8076.eb37770d.jpg"
+  },
+  {
+    "revision": "bee678ecc99a8753af33dda370b27951",
+    "url": "/static/media/qkfeedback.bee678ec.png"
+  },
+  {
+    "revision": "692adb35708d9279ca7eb56c71bfc102",
+    "url": "/static/media/react.692adb35.png"
+  },
+  {
+    "revision": "d154989268aced35ce58e3ffbbdc4f46",
+    "url": "/static/media/see-a-doc-doctor.d1549892.webp"
+  },
+  {
+    "revision": "ff1ea434cb4a33f2503379894ecbf1ab",
+    "url": "/static/media/see-a-doc-patient.ff1ea434.webp"
+  },
+  {
+    "revision": "61a35760ef92856b46dba8e6f72851e8",
+    "url": "/static/media/selenium.61a35760.png"
+  },
+  {
+    "revision": "1f2912ceb53b4799623d332eec1d07fa",
+    "url": "/static/media/sparup-coach.1f2912ce.webp"
+  },
+  {
+    "revision": "52f52565d207e1de1bd071193210a216",
+    "url": "/static/media/sparup-dashboard.52f52565.png"
+  },
+  {
+    "revision": "58662369d9f14595cd94ab4ef59c4f34",
+    "url": "/static/media/sparup-player.58662369.webp"
+  },
+  {
+    "revision": "bff532c54b04ee4ffd431df199f5704b",
+    "url": "/static/media/track.bff532c5.webp"
+  },
+  {
+    "revision": "77b714e86f4f090c77280ebc7cf3435f",
+    "url": "/static/media/volkswagen-jaipur-dashboard.77b714e8.png"
+  },
+  {
+    "revision": "596d6127a881825e9d70a03f054e72d6",
+    "url": "/static/media/volkswagen-jaipur.596d6127.webp"
+  },
+  {
+    "revision": "ad7fee69f39f3026ac0fc1ba6e6b404a",
+    "url": "/static/media/walle8-cca-app.ad7fee69.png"
+  },
+  {
+    "revision": "0e49005f01700c5a32b65ac1f47c83b8",
+    "url": "/static/media/walle8-ro-app.0e49005f.png"
+  },
+  {
+    "revision": "82505c4e54ca311f4ab7597c951e8a92",
+    "url": "/static/media/walle8.82505c4e.png"
+  },
+  {
+    "revision": "b3d9fca9e129f086b78024336344c518",
+    "url": "/static/media/walle8.b3d9fca9.png"
+  }
+]);
