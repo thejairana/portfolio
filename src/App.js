7838db9387122509d53d4b1b57c0f475d6ff2f47
jay.kumar <jay.kumar@elogist.in>
2019-05-29 22:32:15 +0530

    Only Website

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import PrintButton from "./components/PrintButton";
-// import Resume1 from './resumes/format-1/formatt-1';
 import Website from './website/Website';
 function App() {
   return (
     <div>
-      {/* <PrintButton id="resume" label="Download" /> */}
-      {/* <Resume1 /> */}
       <Website />
     </div>
   );
